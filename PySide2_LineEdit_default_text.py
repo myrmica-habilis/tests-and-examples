f7b1f38c79a061fb38d8a58d39700d1beec2ef82
myrmica-habilis <michalportes1@gmail.com>
2018-11-06 19:37:56 +0100

    first

---
+++ PySide2_LineEdit_default_text.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# via https://stackoverflow.com/questions/53100965/qt5-pyside2-lineedit
+
+from PySide2 import QtWidgets
+import time
+
+class MainWindow(QtWidgets.QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.resize(640, 400)
+
+        lineEntry = QtWidgets.QLineEdit(time.strftime("%H:%M:%S", time.localtime()))
+        # default text does not dissappear upon mouse click
+
+        lineEntry.setPlaceholderText("Enter a valid time in 24-hour hh:mm:ss format.")
+        # placeholder text does not dissappear upon mouse click
+        # (it disappears after typing the first char)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(lineEntry)
+        self.setLayout(layout)
+
+    def run(self, app):
+        self.show()
+        app.exec_()
+
+def main():
+    app = QtWidgets.QApplication()
+    appWindow = MainWindow()
+    appWindow.run(app)
+
+if __name__ == "__main__":
+    main()
---
+++ PySide2_QBuffer.data_QByteArray.data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# via https://stackoverflow.com/questions/52291585/pyside2-typeerror-bytes-object-cannot-be-interpreted-as-an-integer
+
+from PySide2.QtCore import QByteArray, QBuffer
+
+testByteArray = bytearray([i for i in range(256)])
+testQByteArray = QByteArray(testByteArray)
+testQBuffer = QBuffer(testQByteArray)
+
+assert(testQBuffer.data().data() == testByteArray)
+
+for name in ["testByteArray", "testQByteArray", "testQBuffer.data()", "testQBuffer.data().data()"]:
+    print("{}: type {}".format(name, type(eval(name))))

f81ea3cb0fbc06a6a45e4e06a725e1801b9945d6
myrmica-habilis <michalportes1@gmail.com>
2018-12-09 15:15:53 +0100

    example code for BmpImagePlugin

---
+++ Pillow_BmpImagePlugin.py
@@ -0,0 +1,20 @@
+# https://stackoverflow.com/questions/53673126
+
+from PIL import BmpImagePlugin
+import hashlib
+from itertools import cycle
+
+keys = hashlib.md5(b"aaaabbbb").digest()
+
+input_image = BmpImagePlugin.BmpImageFile("img/tea.bmp")
+
+# extract pure image data as bytes
+image_data = input_image.tobytes()
+
+# encrypt
+image_data = bytes(a^b for a, b in zip(image_data, cycle(keys)))
+
+# create new image, update with encrypted data and save
+output_image = input_image.copy()
+output_image.frombytes(image_data)
+output_image.save("img/tea-encrypted.bmp")
